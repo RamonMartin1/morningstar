f2dcacdb6e15c0bb9cdece19659e8c60a4fd7c52
Marc Juchli <mail@marcjuch.li>
2020-06-17 00:48:53 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='morningstar',
-    version='0.3.0',
+    version='0.3.1',
     description='Morningstar API Client',
     long_description=long_description,
     long_description_content_type='text/markdown',
